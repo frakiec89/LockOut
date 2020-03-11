377adc9edffb3f0e81333d06a663d0799349ab9b
РИШАТ Ахтямов <rishat-rus@mail.ru>
2020-03-11 18:38:28 +0400

    Добавьте файлы проекта.

---
+++ LockOut/LockOutsController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LockOut
+{
+    /// <summary>
+    /// работа со  входами
+    /// </summary>
+    public class LockOutsController
+    {
+        /// <summary>
+        /// список  входов
+        /// </summary>
+        public  List<LockOutcs> LockOutcs { get; set; }
+
+        /// <summary>
+        /// получаем  список  входов
+        /// </summary>
+        public LockOutsController ()
+        {
+            LockOutcs = Load();// грузим из файла
+        }
+
+        /// <summary>
+        /// сохрание  в файл 
+        /// </summary>
+        public void Save()
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            using (var fille = new FileStream("lockOuts.bin", FileMode.OpenOrCreate))
+            {
+                formatter.Serialize(fille, LockOutcs);
+            }
+          
+        }
+
+        /// <summary>
+        /// добавление  входа  удачного  если пользователь  найден
+        /// </summary>
+        /// <param name="user"></param>
+        public void Add(User user) // принемает  пользователя
+        {
+            LockOutcs.Add(new LockOutcs { Date = DateTime.Now, User = user, Status = true , Login=user.Log  }); // добавляет  вход  в  лист
+            Save(); // сохраняеи  в  файл
+        }
+
+        /// <summary>
+        /// добавляем для неудачнонго входа
+        /// </summary>
+        /// <param name="log"></param>
+        /// <param name="pass"></param>
+        public void Add( string log , string pass) // вводимый логин  и  пароль 
+        {
+            LockOutcs.Add(new LockOutcs { Date = DateTime.Now, Status = false , Login = log , Pass = pass , User = null }); 
+            Save(); // сохраняеи в  файл
+        }
+
+        private List<LockOutcs> Load() // получаем список из  файла 
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            using (var fille = new FileStream("lockOuts.bin", FileMode.OpenOrCreate))
+            {
+
+                try
+                {
+                    var us = formatter.Deserialize(fille);
+                    if (us == null)
+                    {
+                        return new List<LockOutcs>();
+                    }
+                    else
+                    {
+                        return us as List<LockOutcs>;
+                    }
+                }
+                catch
+                {
+                    return new List<LockOutcs>();
+                }
+            }
+        }
+    }
+}
---
+++ LockOut/UserController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+
+namespace LockOut
+{
+    
+    /// <summary>
+    /// работа с пользователями  
+    /// </summary>
+    public class UserController
+    {
+        /// <summary>
+        /// список  пользователей
+        /// </summary>
+       public List<User>  Users{ get; set; }
+
+        /// <summary>
+        /// создает  новых   юзеров  по типу   User1  пароль 1
+        /// </summary>
+        /// <param name="count"></param>
+        private UserController ( int  count)
+        {
+            Users = new List<User>(); 
+
+            for (int i = 1; i<= count; i++) // создаем по  одному   - добавляем в  лист  
+            {
+                Users.Add(new User { Name = i + " User", Log = "User" + i, Pass = i.ToString() }); 
+            }
+
+            Save(); // сохранияем  в файл
+       }
+
+
+        /// <summary>
+        /// получаем пользователей 
+        /// </summary>
+        public UserController ()
+        {
+            Users = Load(); // загружаем пользователей  из  файла 
+            if(Users.Count==0) // если пользователей  в  файле  нет  
+            {
+                UserController user = new UserController(10); // создаем   новых пользователей , записываем их  в файд
+                Users = Load(); // забираем  из файла 
+            }
+        }
+        public  void Save () // сохраняем список  юзеров  в  файл
+        {
+            BinaryFormatter formatter = new BinaryFormatter(); // формтарор
+            using (var fille = new FileStream("user.bin", FileMode.OpenOrCreate)) // поток данных  в  файл  
+            {
+              formatter .Serialize(fille, Users); // сириализация 
+            }
+
+        }
+
+        public List <User> Load() //   получаем список  юзеров из файла 
+        {
+            BinaryFormatter formatter = new BinaryFormatter(); // формтарор
+
+            using (var fille = new FileStream("user.bin", FileMode.OpenOrCreate)) // поток данных  из  файла   
+            {
+
+                try
+                {
+                    var us = formatter.Deserialize(fille); // забираем объект  из  файла
+                    if (us == null) // если файл  пустой
+                    {
+                        return new List<User>(); // создаем пустой  лист
+                    }
+                    else
+                    {
+                        return us as List<User>; // преобразуем объект  в  список листов 
+                    }
+                }
+                catch
+                {
+                    return new List<User>(); // если поток данных пустой создаем пустой  лист
+                }
+            }
+
+        }
+
+
+    }
+}
